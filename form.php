7128469599af2e1b655f766ef41ce1937801553b
rtviwe <rtviwe@gmail.com>
2019-04-15 20:44:13 +0300

    initial commit

---
+++ form.php
@@ -0,0 +1,8 @@
+<?php
+echo "
+<h2>nslookup</h2>
+<form action='form.php' method='POST'>
+Доменное имя<br>
+<input type=text name='domain_name'><br>
+<input type=submit value='Отправить'>
+</form>";
