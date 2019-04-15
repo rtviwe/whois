86285867cf667dae77bf0c39872ef3551eff8560
rtviwe <rtviwe@gmail.com>
2019-04-15 20:55:48 +0300

    first attempt

--- data.php
+++ data.php
@@ -1,1 +1,7 @@
 <?php
+$domain_name = $_POST["domain_name"];
+$ip = gethostbyname($domain_name);
+echo "
+Доменное имя: $domain_name<br />
+IP адрес: $ip<br />
+";
--- form.php
+++ form.php
@@ -1,7 +1,7 @@
 <?php
 echo "
 <h2>nslookup</h2>
-<form action='form.php' method='POST'>
+<form action='data.php' method='POST'>
 Доменное имя<br>
 <input type=text name='domain_name'><br>
 <input type=submit value='Отправить'>
