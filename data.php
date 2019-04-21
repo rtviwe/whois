81085e9a8d622ada9bc32bc318086752bf9abd5c
rtviwe <rtviwe@gmail.com>
2019-04-21 23:09:24 +0300

    attemp to connect db

--- data.php
+++ data.php
@@ -1,7 +1,24 @@
 <?php
-$domain_name = $_POST["domain_name"];
+if (isset($_POST['submit'])) {
+    $domain_name = $_POST["domain_name"];
+} else {
+    $domain_name = "ошибка: нет доменного имени";
+}
+
+$start = microtime();
 $ip = gethostbyname($domain_name);
+$end = microtime();
+$time = $end - $start;
+
 echo "
 Доменное имя: $domain_name<br />
 IP адрес: $ip<br />
 ";
+
+$link = mysqli_connect("%", "php_user", "qwe123", "log");
+
+// TODO insert log
+// mysqli_query();
+
+mysqli_close($link);
+
