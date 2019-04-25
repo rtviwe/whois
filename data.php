78366303055ad7c6b6fe3658811b978be73ae12a
rtviwe <rtviwe@gmail.com>
2019-04-25 20:13:07 +0300

    Fix get domain_name

--- data.php
+++ data.php
@@ -1,6 +1,6 @@
 <?php
-if (isset($_POST['submit'])) {
-    $domain_name = $_POST["domain_name"];
+if (isset($_POST['domain_name'])) {
+    $domain_name = $_POST['domain_name'];
 } else {
     $domain_name = "ошибка: нет доменного имени";
 }
@@ -10,15 +10,19 @@ $ip = gethostbyname($domain_name);
 $end = microtime();
 $time = $end - $start;
 
+// TODO find contacts
+$contacts = "";
+
 echo "
 Доменное имя: $domain_name<br />
 IP адрес: $ip<br />
+Контактные данные: $contacts<br />
 ";
 
-$link = mysqli_connect("%", "php_user", "qwe123", "log");
+// TODO connect db
+//$link = mysqli_connect("%", "php_user", "qwe123", "log");
 
 // TODO insert log
 // mysqli_query();
 
-mysqli_close($link);
-
+//mysqli_close($link);
