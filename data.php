7837b5055ede4ff58effcacdc40aad8347ac51c2
rtviwe <rtviwe@gmail.com>
2019-04-29 22:04:17 +0300

    dadata

--- data.php
+++ data.php
@@ -1,17 +1,22 @@
 <?php
-if (isset($_POST['domain_name'])) {
-    $domain_name = $_POST['domain_name'];
-} else {
-    $domain_name = "ошибка: нет доменного имени";
-}
+$API_KEY = "1cf94e840097454101fdbb0a52ce1ec7ee7ce6ea";
+$domain_name = $_POST['domain_name'];
 
 $start = microtime();
 $ip = gethostbyname($domain_name);
 $end = microtime();
 $time = $end - $start;
 
-// TODO find contacts
-$contacts = "";
+// https://dadata.ru/api/detect_address_by_ip/
+$myCurl = curl_init();
+curl_setopt_array($myCurl, array(
+    CURLOPT_URL => "https://suggestions.dadata.ru/suggestions/api/4_1/rs/iplocate/address",
+    CURLOPT_HTTPHEADER => array("Authorization: $API_KEY"),
+    CURLOPT_POSTFIELDS => http_build_query(array("ip" => $ip))
+));
+$response = curl_exec($myCurl);
+curl_close($myCurl);
+$contacts = $response;
 
 echo "
 Доменное имя: $domain_name<br />
