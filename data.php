03507dcbe12c3055ae4e8fa2dc58aa0e8ac8125a
rtviwe <rtviwe@gmail.com>
2019-05-02 17:09:15 +0300

    Русификация

--- data.php
+++ data.php
@@ -29,6 +29,33 @@ function get_whois($ip, $server = "whois.arin.net")
     }
 }
 
+function translate($text)
+{
+    $text = preg_replace("/OrgName/", "Название организации", $text, 1);
+    $text = preg_replace("/OrgId/", "Id организации", $text, 1);
+    $text = preg_replace("/Address/", "Адрес", $text, 1);
+    $text = preg_replace("/City/", "Город", $text, 1);
+    $text = preg_replace("/StateProv/", "Штат", $text, 1);
+    $text = preg_replace("/PostalCode/", "Индекс", $text, 1);
+    $text = preg_replace("/Country/", "Страна", $text, 1);
+    $text = preg_replace("/RegDate/", "Дата регистрации", $text, 1);
+    $text = preg_replace("/Updated/", "Обновлено", $text, 1);
+    $text = preg_replace("/Ref/", "Ссылка", $text, 1);
+    $text = preg_replace("/ReferralServer/", "Сервер", $text, 1);
+    $text = preg_replace("/ResourceLink/", "Ссылка на ресурсы", $text, 1);
+    $text = preg_replace("/OrgAbuseHandle/", "Владелец огранизации", $text, 1);
+    $text = preg_replace("/OrgAbuseName/", "Название владельца организации", $text, 1);
+    $text = preg_replace("/OrgAbusePhone/", "Номер владельца организации", $text, 1);
+    $text = preg_replace("/OrgAbuseEmail/", "Почта владельца организации", $text, 1);
+    $text = preg_replace("/OrgAbuseRef/", "Сслыка владельца организации", $text, 1);
+    $text = preg_replace("/OrgTechHandle/", "Организация", $text, 1);
+    $text = preg_replace("/OrgTechName/", "Название организации", $text, 1);
+    $text = preg_replace("/OrgTechPhone/", "Номер организации", $text, 1);
+    $text = preg_replace("/OrgTechEmail/", "Почта организации", $text, 1);
+    $text = preg_replace("/OrgTechRef/", "Ссылка организации", $text, 1);
+    return $text;
+}
+
 $domain_name = $_POST['domain_name'];
 
 $start = microtime_float();
@@ -41,17 +68,18 @@ $t1 = strpos($whois, "OrgName");
 $temp = substr($whois, $t1);
 $t2 = strpos($temp, "# ARIN WHOIS");
 $contacts = substr($temp, 0, $t2 - 10);
+$contacts = translate($contacts);
 
 echo "
 Доменное имя: $domain_name<br />
 IP адрес: $ip<br />
-Контактные данные:<br /> $contacts<br />
+$contacts<br />
 ";
 
 // TODO connect db
-//$link = mysqli_connect("%", "php_user", "qwe123", "log");
+//$link = mysqli_connect("localhost", "php_user", "qwe123", "log");
 
 // TODO insert log
-// mysqli_query();
+//mysqli_query();
 
 //mysqli_close($link);
