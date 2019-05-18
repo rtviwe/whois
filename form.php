4e84bc6af50bfbb75abb9156429d432f486c6288
rtviwe <rtviwe@gmail.com>
2019-05-18 18:38:40 +0300

    remove old files

--- form.php
+++
@@ -1,8 +0,0 @@
-<?php
-echo "
-<h2>nslookup</h2>
-<form action='data.php' method='POST'>
-Доменное имя<br>
-<input type=text name='domain_name'><br>
-<input type=submit value='Отправить'>
-</form>";
--- xampp.sh
+++
@@ -1,3 +0,0 @@
-sudo /etc/init.d/apache2 stop
-sudo /etc/init.d/mysql stop
-sudo /opt/lampp/lampp restart
